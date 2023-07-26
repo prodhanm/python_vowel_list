3162d9eeda781073fd744a1960573185737d6406
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-26 17:44:21 -0400

    set up and tested code.

---
+++ vowel_list.py
@@ -0,0 +1,15 @@
+words = ['abstemious', 'luxury', 'onomotopiea', 'existence', 'death', 'Islam', 'president']
+
+def vowel(words):
+    vowels = 'aeiou'
+    for word in words:
+        v_ct = 0
+        v_li = []
+        for w in range(len(word)):
+            if word[w].lower() in vowels:
+                v_ct += 1
+                v_li.append(word[w])
+        print(f"There are {v_ct} vowels in {word}.")
+        print(f"{word} = {list(set(v_li))}")
+    
+vowel(words)
